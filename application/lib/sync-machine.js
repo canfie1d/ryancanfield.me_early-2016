684f517de469aa1c0346cb8613938096f0711d63
Ben Youngblood <ben@synapsestudios.com>
2014-03-11 10:14:33 -0700

    fix indentation

--- application/lib/sync-machine.js
+++ application/lib/sync-machine.js
@@ -4,102 +4,102 @@
 define([
 ], function(
 ) {
-        'use strict';
+    'use strict';
 
-        var event, handlerFn;
+    var event, handlerFn;
 
-        var UNSYNCED = 'unsynced',
-            SYNCING = 'syncing',
-            SYNCED = 'synced',
-            STATE_CHANGE = 'syncStateChange';
+    var UNSYNCED = 'unsynced',
+        SYNCING = 'syncing',
+        SYNCED = 'synced',
+        STATE_CHANGE = 'syncStateChange';
 
-        var SyncMachine = {
+    var SyncMachine = {
 
-            _syncState         : UNSYNCED,
-            _previousSyncState : null,
+        _syncState         : UNSYNCED,
+        _previousSyncState : null,
 
-            syncState: function() {
-                return this._syncState;
-            },
+        syncState: function() {
+            return this._syncState;
+        },
 
-            isUnsynced: function() {
-                return this._syncState === UNSYNCED;
-            },
+        isUnsynced: function() {
+            return this._syncState === UNSYNCED;
+        },
 
-            isSynced: function() {
-                return this._syncState === SYNCED;
-            },
+        isSynced: function() {
+            return this._syncState === SYNCED;
+        },
 
-            isSyncing: function() {
-                return this._syncState === SYNCING;
-            },
+        isSyncing: function() {
+            return this._syncState === SYNCING;
+        },
 
-            unsync: function() {
-                var state;
+        unsync: function() {
+            var state;
 
-                if ((state = this._syncState) === SYNCING || state === SYNCED) {
-                    this._previousSync = this._syncState;
-                    this._syncState    = UNSYNCED;
+            if ((state = this._syncState) === SYNCING || state === SYNCED) {
+                this._previousSync = this._syncState;
+                this._syncState    = UNSYNCED;
 
-                    this.trigger(this._syncState, this, this._syncState);
-                    this.trigger(STATE_CHANGE, this, this._syncState);
-                }
-            },
-            beginSync: function() {
-                var state;
+                this.trigger(this._syncState, this, this._syncState);
+                this.trigger(STATE_CHANGE, this, this._syncState);
+            }
+        },
+        beginSync: function() {
+            var state;
 
-                if ((state = this._syncState) === UNSYNCED || state === SYNCED) {
-                    this._previousSync = this._syncState;
-                    this._syncState    = SYNCING;
+            if ((state = this._syncState) === UNSYNCED || state === SYNCED) {
+                this._previousSync = this._syncState;
+                this._syncState    = SYNCING;
 
-                    this.trigger(this._syncState, this, this._syncState);
-                    this.trigger(STATE_CHANGE, this, this._syncState);
-                }
-            },
-            finishSync: function() {
-                if (this._syncState === SYNCING) {
-                    this._previousSync = this._syncState;
-                    this._syncState    = SYNCED;
+                this.trigger(this._syncState, this, this._syncState);
+                this.trigger(STATE_CHANGE, this, this._syncState);
+            }
+        },
+        finishSync: function() {
+            if (this._syncState === SYNCING) {
+                this._previousSync = this._syncState;
+                this._syncState    = SYNCED;
 
-                    this.trigger(this._syncState, this, this._syncState);
-                    this.trigger(STATE_CHANGE, this, this._syncState);
-                }
-            },
-            abortSync: function() {
-                if (this._syncState === SYNCING) {
-                    this._syncState    = this._previousSync;
-                    this._previousSync = this._syncState;
+                this.trigger(this._syncState, this, this._syncState);
+                this.trigger(STATE_CHANGE, this, this._syncState);
+            }
+        },
+        abortSync: function() {
+            if (this._syncState === SYNCING) {
+                this._syncState    = this._previousSync;
+                this._previousSync = this._syncState;
 
-                    this.trigger(this._syncState, this, this._syncState);
-                    this.trigger(STATE_CHANGE, this, this._syncState);
-                }
+                this.trigger(this._syncState, this, this._syncState);
+                this.trigger(STATE_CHANGE, this, this._syncState);
+            }
+        }
+    };
+
+    var states = [UNSYNCED, SYNCING, SYNCED, STATE_CHANGE];
+
+    handlerFn = function(event) {
+        return SyncMachine[event] = function(callback, context) {
+            if (context == null) {
+                context = this;
+            }
+
+            this.on(event, callback, context);
+
+            if (this._syncState === event) {
+                return callback.call(context);
             }
         };
+    };
 
-        var states = [UNSYNCED, SYNCING, SYNCED, STATE_CHANGE];
+    for (var i = 0, len = states.length; i < len; i++) {
+        event = states[i];
+        handlerFn(event);
+    }
 
-        handlerFn = function(event) {
-            return SyncMachine[event] = function(callback, context) {
-                if (context == null) {
-                    context = this;
-                }
+    if (typeof Object.freeze === "function") {
+        Object.freeze(SyncMachine);
+    }
 
-                this.on(event, callback, context);
-
-                if (this._syncState === event) {
-                    return callback.call(context);
-                }
-            };
-        };
-        for (var i = 0, var len = states.length; i < len; i++) {
-            event = states[i];
-            handlerFn(event);
-        }
-
-        if (typeof Object.freeze === "function") {
-            Object.freeze(SyncMachine);
-        }
-
-        module.exports = SyncMachine;
-
+    return SyncMachine;
 });
