6c8d0ff0a04f67c25ba65f9ee6a7531b7ada23bf
Zachary Chavez <zach@synapsestudios.com>
2014-03-26 14:31:45 -0700

    Add links for Google login and linking.

--- application/jsx/home.jsx
+++ application/jsx/home.jsx
@@ -41,7 +41,12 @@ define([
                         </p>
                         <p>
                             <a href="http://project.vm/social-login/github/link">Link GitHub account</a><br />
-                            <a href="http://project.vm/social-login/facebook/link">Link Facebook account</a>
+                            <a href="http://project.vm/social-login/facebook/link">Link Facebook account</a><br />
+                            {/*
+                                Google only allows 'localhost" as a non-public domain for callback URLs.
+                                This link won't work unless the URLs in models/user.js are also changed to 'localhost'.
+                            */}
+                            <a href="http://localhost/social-login/google/link">Link Google account</a><br />
                         </p>
                     </div>
                 );
@@ -58,6 +63,10 @@ define([
                         </p>
                         <p>
                             Or <a href="http://project.vm/social-login/facebook">log in with Facebook</a>.
+                        </p>
+                        <p>
+                            {/* Google only allows "localhost" as a non-public domain for callback URLs. */}
+                            Or <a href="http://localhost/social-login/google">log in with Google</a>.
                         </p>
                     </div>
                 );
