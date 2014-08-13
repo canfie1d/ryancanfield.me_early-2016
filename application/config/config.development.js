cebc0de0d0500dfb0af7b17b3bc9b814a5ef5b38
Zachary Chavez <zach@synapsestudios.com>
2014-08-13 11:48:17 -0700

    Refs #17 - Remove unused loginWithGithubUrl value from config.

--- application/config/config.development.js
+++ application/config/config.development.js
@@ -3,7 +3,5 @@ module.exports = {
         hostname  : 'api.project.vm',
         port      : 80,
         client_id : 'js-client'
-    },
-
-    loginWithGithubUrl : 'http://api.project.vm/social-login/github'
+    }
 };
--- application/config/config.production.js
+++ application/config/config.production.js
@@ -2,7 +2,5 @@ module.exports = {
     api : {
         hostname : 'api.project.vm',
         port     : 80
-    },
-
-    loginWithGithubUrl : 'http://api.project.vm/social-login/github'
+    }
 };
