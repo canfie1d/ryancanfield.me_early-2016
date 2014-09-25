471b8427410c28d03b7a0bc885de4097fa93d837
Zachary Chavez <zach@synapsestudios.com>
2014-09-25 15:44:12 -0700

    Refs #27 - Add support for environment-dependent routes and a blank style guide page.

--- application/routes.js
+++ application/routes.js
@@ -1,4 +1,5 @@
 /** @jsx React.DOM */
+/* globals __ENVIRONMENT__ */
 'use strict';
 
 var flux  = require('./flux');
@@ -8,6 +9,7 @@ var SiteLayout       = require('./ui/layouts/site');
 var HomePage         = require('./ui/pages/home');
 var LoginPage        = require('./ui/pages/login');
 var RegistrationPage = require('./ui/pages/register');
+var StyleGuidePage   = require('./ui/pages/style-guide');
 var NotFoundPage     = require('./ui/pages/404');
 
 var props = function(name, path, handler, props)
@@ -21,10 +23,22 @@ var props = function(name, path, handler, props)
     return props;
 };
 
+var getEnvironmentDependentRoutes = function()
+{
+    var routes = [];
+
+    if (__ENVIRONMENT__ !== 'production') {
+        routes.push(Route(props('style-guide', '/style-guide', StyleGuidePage, {key : 'style-guide'})));
+    }
+
+    return routes;
+};
+
 module.exports = Route(
     {handler : SiteLayout, location : 'history', flux : flux},
     Route(props('home', '/', HomePage)),
     Route(props('login', '/login', LoginPage)),
     Route(props('register', '/register', RegistrationPage)),
+    getEnvironmentDependentRoutes(),
     Route(props('404', '*', NotFoundPage))
 );
---
+++ application/ui/pages/style-guide.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+
+    displayName : 'StyleGuide',
+
+    render : function()
+    {
+        return (
+            <div></div>
+        );
+    }
+
+});
