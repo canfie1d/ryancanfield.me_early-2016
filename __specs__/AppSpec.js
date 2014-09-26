fffaf6e9abc60adb97566f9a7e157344066b427c
Andrew Reida <andrew@syn0.com>
2014-09-26 10:57:23 -0700

    #28 | Adding example of testing clicking/routing to make beforeEach worthwhile

--- __specs__/AppSpec.js
+++ __specs__/AppSpec.js
@@ -13,5 +13,15 @@ describe('Application', function() {
         it('loads the home page', function() {
             expectToBeOnHomepage(page);
         });
+
+        it('links to login page', function(done) {
+            page.click('a[href="/login"]');
+
+            setTimeout(function() {
+                expect(page.find('label[for="email"]')).toHaveText('Email:');
+                expect(page.find('label[for="password"]')).toHaveText('Password:');
+                done();
+            });
+        });
     });
 });
