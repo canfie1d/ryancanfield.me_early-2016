5daccdf0455157e702df7ebf08741c0aa63f9bdd
Andrew Reida <andrew@syn0.com>
2014-09-25 18:58:40 -0700

    #28 | Adding shims for CI

--- application/bootstrap.js
+++ application/bootstrap.js
@@ -1,5 +1,11 @@
-/* global window */
+
+/* global __ENVIRONMENT__ */
 'use strict';
+
+if (__ENVIRONMENT__ === 'ci') {
+    require('es5-shim');
+    require('../node_modules/disyntegration/shim/click');
+}
 
 var React  = require('react');
 var routes = require('./routes');
