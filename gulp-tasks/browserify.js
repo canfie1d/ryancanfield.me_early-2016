036e6a4e7c2bff438716bfd3f3961c1f8d05777d
Andrew Reida <andrew@syn0.com>
2014-10-01 12:33:27 -0700

    #28 | removing jest

--- gulp-tasks/browserify.js
+++ gulp-tasks/browserify.js
@@ -1,7 +1,9 @@
 /* jshint node: true */
 'use strict';
 
+var _          = require('underscore');
 var browserify = require('browserify');
+var proxyquire = require('proxyquireify');
 var connect    = require('gulp-connect');
 var glob       = require('glob');
 var gulp       = require('gulp');
@@ -12,6 +14,7 @@ var source     = require('vinyl-source-stream');
 var streamify  = require('gulp-streamify');
 var uglify     = require('gulp-uglify');
 var watchify   = require('watchify');
+var watchr     = require('watchr');
 
 var error = require('./error');
 
@@ -70,47 +73,68 @@ gulp.task('watchify:app', function() {
 });
 
 gulp.task('watchify:test', function () {
-    var backend, bundler, entries, env, path, rebundle;
+    var backend, bundle, bundler, entries, env, path, rebundle;
 
     backend = gutil.env.backend || '';
     env     = gutil.env.env;
-    path    = gutil.env.path || './__react-tests__/**/*.js*';
-    entries = glob.sync(path);
+    path    = gutil.env.path || './__tests__/**/*.js*';
 
-    bundler = watchify(
-        browserify({
-            debug        : (env !== 'production'),
-            entries      : entries,
-            extensions   : ['.js', '.jsx'],
-            cache        : {},
-            packageCache : {},
-            fullPaths    : true
-        })
-        .ignore('./__react-tests__/globals.js')
-        .transform(reactify)
-        .on('log', gutil.log)
-    );
+    bundle = function() {
+        entries = glob.sync(path);
 
-    rebundle = function() {
-        return bundler.bundle()
-            .on('error', error('browserify:test'))
-            .pipe(source('tests.js'))
-            .pipe(streamify(
-                replace({
-                    patterns : [{
-                        match       : /__ENVIRONMENT__/g,
-                        replacement : '\''+env+'\''
-                    }, {
-                        match       : /__BACKEND__/g,
-                        replacement : '\''+backend+'\''
-                    }]
-                })
-            ))
-            .pipe(gulp.dest('./test'))
-            .pipe(connect.reload(true));
+        bundler = watchify(
+            browserify({
+                debug        : (env !== 'production'),
+                entries      : entries,
+                extensions   : ['.js', '.jsx'],
+                cache        : {},
+                packageCache : {},
+                fullPaths    : true
+            })
+            .plugin(proxyquire.plugin)
+            .ignore('./__tests__/globals.js')
+            .transform(reactify)
+            .on('log', gutil.log)
+        );
+
+        rebundle = function() {
+            return bundler.bundle()
+                .on('error', error('browserify:test'))
+                .pipe(source('tests.js'))
+                .pipe(streamify(
+                    replace({
+                        patterns : [{
+                            match       : /__ENVIRONMENT__/g,
+                            replacement : '\''+env+'\''
+                        }, {
+                            match       : /__BACKEND__/g,
+                            replacement : '\''+backend+'\''
+                        }]
+                    })
+                ))
+                .pipe(gulp.dest('./test'))
+                .pipe(connect.reload(true));
+        };
+
+        bundler.on('update', function(paths) {
+            // Prevent double reload. Watchify updates on new files but doesn't do anything
+            if (_.intersection(entries, paths).length) {
+                rebundle();
+            }
+        });
+
+        return rebundle();
     };
 
-    bundler.on('update', rebundle);
+    watchr.watch({
+        path     : './__tests__',
+        listener : function(changeType) {
+            // Ignore changes to existing files
+            if (changeType !== 'update') {
+               bundle();
+            }
+        }
+    });
 
-    return rebundle();
+    return bundle();
 });
