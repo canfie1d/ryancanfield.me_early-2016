242a70b681f97eb8b64bb2920c0168957f656a59
Andrew Reida <andrew@syn0.com>
2014-10-08 11:10:45 -0700

    #52 | Removing specs that can’t be run anymore

--- test-helpers/spec-helper.js
+++ test-helpers/spec-helper.js
@@ -1,15 +1,5 @@
 var specHelper = {};
 
-specHelper.emailField       = 'input[name="email"]';
-specHelper.formErrorMessage = 'p[data-reactid=".0.1.0.6"]';
-specHelper.formLabel        = 'p[data-reactid=".0.1.0.0"]';
-specHelper.headerText       = 'p[data-reactid=".0.0.0.0"]';
-specHelper.homeLink         = 'a[href="/"]';
-specHelper.loginButton      = 'input[value="Log in"]';
-specHelper.loginLink        = 'a[href="/login"]';
-specHelper.passwordField    = 'input[name="password"]';
-specHelper.registrationLink = 'a[href="/register"]';
-
 specHelper.getDataLoadedFunction = function(page) {
     return function() {
         var reactElement = 'div[data-reactid=".0"]';
