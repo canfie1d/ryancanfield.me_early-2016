32560d8d81891637b2ec078fa5967fcbc4d9d005
Andrew <andrew@syn0.com>
2015-02-14 12:47:15 -0700

    #69 | Better code cosolidation

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,20 +1,10 @@
 /* jshint expr: true, globalstrict: true */
 'use strict';
 
-// Ensure that chai accepts should-style assertions and is integrated with sinon.
-var chai      = require('chai');
-var sinonChai = require('sinon-chai');
-
-chai.should();
-chai.use(sinonChai);
+require('./index');
 
 require('../node_modules/mocha/mocha.css');
 require('../node_modules/mocha/mocha.js');
 require('../node_modules/sinon/pkg/sinon.js');
 require('./favicon.js');
 require('./favicon.png');
-
-// Require all modules ending in '-test'
-var context = require.context('.', true, /\-test$/);
-
-context.keys().forEach(context);
