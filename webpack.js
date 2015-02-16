e2567b0b21452ae950818324d0aebf31d1ef3d90
Andrew Reida <andrew@syn0.com>
2015-02-16 10:46:34 -0700

    #69 | Using middleware to serve all urls with dev server

--- webpack.js
+++ webpack.js
@@ -1,12 +1,26 @@
+var path             = require('path');
+var request          = require('request');
 var WebpackDevServer = require('webpack-dev-server');
 var webpack          = require('webpack');
 var config           = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
     contentBase : __dirname + '/build',
     hot         : true,
     noInfo      : true
-}).listen(9000, 'localhost', function (err, result) {
+});
+
+server.use(function pushStateHook(req, res, next) {
+    var ext = path.extname(req.url);
+
+    if ((ext === '' || ext === '.html') && req.url !== '/') {
+        req.pipe(request('http://localhost:9000')).pipe(res);
+    } else {
+        next();
+    }
+});
+
+server.listen(9000, 'localhost', function (err, result) {
     if (err) {
         console.log(err);
     }
