fd7f258d0553430484db001c247a7fc3d3de7bb8
Adam Gerbens <adam@synapsestudios.com>
2015-02-22 13:26:38 -0700

    Refs #75 | Add i18n for typography and icons sg sections

--- application/ui/components/style-guide/sections/sg-icons.jsx
+++ application/ui/components/style-guide/sections/sg-icons.jsx
@@ -1,87 +1,91 @@
 /* jshint globalstrict: true */
 'use strict';
 
-var React = require('react');
-var Icon  = require('../../icon/icon');
+var React     = require('react');
+var Icon      = require('../../icon/icon');
+var ReactIntl = require('react-intl');
+var IntlMixin = ReactIntl.IntlMixin;
 
 module.exports = React.createClass({
 
     displayName : 'Icons',
 
+    mixins: [ IntlMixin ],
+
     render : function()
     {
         return (
             <div className='sg-page'>
-                <h1 className='sg-h1'>{'Icons'}</h1>
+                <h1 className='sg-h1'>{this.getIntlMessage('sg.section.Icons')}</h1>
                 <div className='sg-icons'>
                     <span className='sg-icon'>
                         <Icon icon='navicon' />
                         <span className='sg-icon__name'>
-                            <strong className='strong'>Icon: </strong>
-                            {'navicon'}
+                            <strong className='strong'>{this.getIntlMessage('sg.icons.icon')}</strong>
+                            {this.getIntlMessage('sg.icons.navicon')}
                         </span>
                     </span>
                     <span className='sg-icon'>
                         <Icon icon='caret' />
                         <span className='sg-icon__name'>
-                            <strong className='strong'>Icon: </strong>
-                            {'caret'}
+                            <strong className='strong'>{this.getIntlMessage('sg.icons.icon')}</strong>
+                            {this.getIntlMessage('sg.icons.caret')}
                         </span>
                     </span>
                 </div>
                 <hr className='sg-hr' />
                 <div className='row'>
                     <div className='medium-4 columns'>
-                        <h2 className='sg-h2'>{'Icon sizes'}</h2>
-                        <Icon icon='navicon' size='x-small' /> Extra Small
+                        <h2 className='sg-h2'>{this.getIntlMessage('sg.icons.icon-sizes')}</h2>
+                        <Icon icon='navicon' size='x-small' /> {this.getIntlMessage('sg.icons.xs')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='small' /> Small
+                        <Icon icon='navicon' size='small' /> {this.getIntlMessage('sg.icons.s')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='medium' /> Medium
+                        <Icon icon='navicon' size='medium' /> {this.getIntlMessage('sg.icons.m')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='large' /> Large
+                        <Icon icon='navicon' size='large' /> {this.getIntlMessage('sg.icons.l')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='x-large' /> Extra Large
-                        <br />
-                        <br />
-                    </div>
-                    <div className='medium-4 columns'>
-                        <h2 className='sg-h2'>{'Icon rotation'}</h2>
-                        <Icon icon='caret' size='small' rotate={0} /> 0deg Rotation
-                        <br />
-                        <br />
-                        <Icon icon='caret' size='small' rotate={45} /> 45deg Rotation
-                        <br />
-                        <br />
-                        <Icon icon='caret' size='small' rotate={90} /> 90deg Rotation
-                        <br />
-                        <br />
-                        <Icon icon='caret' size='small' rotate={180} /> 180deg Rotation
-                        <br />
-                        <br />
-                        <Icon icon='caret' size='small' rotate={270} /> 270deg Rotation
+                        <Icon icon='navicon' size='x-large' /> {this.getIntlMessage('sg.icons.xl')}
                         <br />
                         <br />
                     </div>
                     <div className='medium-4 columns'>
-                        <h2 className='sg-h2'>{'Icon colors'}</h2>
-                        <Icon icon='navicon' size='small' color='black' /> Black fill
+                        <h2 className='sg-h2'>{this.getIntlMessage('sg.icons.icon-rotation')}</h2>
+                        <Icon icon='caret' size='small' rotate={0} /> {this.getIntlMessage('sg.icons.0deg')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='small' color='white' /> White fill
+                        <Icon icon='caret' size='small' rotate={45} /> {this.getIntlMessage('sg.icons.45deg')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='small' color='primary' /> Primary fill
+                        <Icon icon='caret' size='small' rotate={90} /> {this.getIntlMessage('sg.icons.90deg')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='small' color='secondary' /> Secondary fill
+                        <Icon icon='caret' size='small' rotate={180} /> {this.getIntlMessage('sg.icons.180deg')}
                         <br />
                         <br />
-                        <Icon icon='navicon' size='small' color='tertiary' /> Tertiary fill
+                        <Icon icon='caret' size='small' rotate={270} /> {this.getIntlMessage('sg.icons.270deg')}
+                        <br />
+                        <br />
+                    </div>
+                    <div className='medium-4 columns'>
+                        <h2 className='sg-h2'>{this.getIntlMessage('sg.icons.icon-colors')}</h2>
+                        <Icon icon='navicon' size='small' color='black' /> {this.getIntlMessage('sg.icons.black')}
+                        <br />
+                        <br />
+                        <Icon icon='navicon' size='small' color='white' /> {this.getIntlMessage('sg.icons.white')}
+                        <br />
+                        <br />
+                        <Icon icon='navicon' size='small' color='primary' /> {this.getIntlMessage('sg.icons.primary')}
+                        <br />
+                        <br />
+                        <Icon icon='navicon' size='small' color='secondary' /> {this.getIntlMessage('sg.icons.secondary')}
+                        <br />
+                        <br />
+                        <Icon icon='navicon' size='small' color='tertiary' /> {this.getIntlMessage('sg.icons.tertiary')}
                         <br />
                         <br />
                     </div>
--- application/ui/components/style-guide/sections/sg-typography.jsx
+++ application/ui/components/style-guide/sections/sg-typography.jsx
@@ -1,44 +1,51 @@
 /* jshint globalstrict: true */
 'use strict';
 
-var React  = require('react');
+var React     = require('react');
+var ReactIntl = require('react-intl');
+var IntlMixin = ReactIntl.IntlMixin;
 
 module.exports = React.createClass({
 
     displayName : 'Typography',
 
+    mixins: [ IntlMixin ],
+
     render : function()
     {
         return (
             <div className='sg-page'>
-                <h1 className='sg-h1'>{'Typography'}</h1>
-                <h2 className='sg-h2'>{'Headers'}</h2>
+                <h1 className='sg-h1'>{this.getIntlMessage('sg.section.Typography')}</h1>
+                <h2 className='sg-h2'>{this.getIntlMessage('sg.typography.headers')}</h2>
 
-                <h1 className='h1'>{'Heading H1'}</h1>
-                <h2 className='h2'>{'Heading H2'}</h2>
-                <h3 className='h3'>{'Heading H3'}</h3>
-                <h4 className='h4'>{'Heading H4'}</h4>
-                <h5 className='h5'>{'Heading H5'}</h5>
-                <h6 className='h6'>{'Heading H6'}</h6>
+                <h1 className='h1'>{this.getIntlMessage('sg.typography.h1')}</h1>
+                <h2 className='h2'>{this.getIntlMessage('sg.typography.h2')}</h2>
+                <h3 className='h3'>{this.getIntlMessage('sg.typography.h3')}</h3>
+                <h4 className='h4'>{this.getIntlMessage('sg.typography.h4')}</h4>
+                <h5 className='h5'>{this.getIntlMessage('sg.typography.h5')}</h5>
+                <h6 className='h6'>{this.getIntlMessage('sg.typography.h6')}</h6>
 
                 <hr className='sg-hr' />
 
-                <h2 className='sg-h2'>{'Grouping elements'}</h2>
-                <p className='p'><strong className='strong'>Paragraph</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta vehicula mi, vel placerat sem viverra in. Vivamus convallis semper est nec placerat. Phasellus laoreet nibh nec ligula dapibus rutrum id vel purus. Mauris eget fermentum felis. Morbi iaculis sapien et libero accumsan, sagittis maximus erat laoreet. Curabitur vitae orci commodo libero convallis molestie. Fusce tincidunt urna varius nulla egestas aliquet. Donec porttitor, mi fermentum commodo hendrerit, velit nisl venenatis lorem, quis porttitor risus elit quis orci.</p>
+                <h2 className='sg-h2'>{this.getIntlMessage('sg.typography.grouping')}</h2>
+                <p className='p'><strong className='strong'>{this.getIntlMessage('sg.typography.p')}</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta vehicula mi, vel placerat sem viverra in. Vivamus convallis semper est nec placerat. Phasellus laoreet nibh nec ligula dapibus rutrum id vel purus. Mauris eget fermentum felis. Morbi iaculis sapien et libero accumsan, sagittis maximus erat laoreet. Curabitur vitae orci commodo libero convallis molestie. Fusce tincidunt urna varius nulla egestas aliquet. Donec porttitor, mi fermentum commodo hendrerit, velit nisl venenatis lorem, quis porttitor risus elit quis orci.</p>
 
                 <blockquote className='blockquote'>
-                    <strong className='strong'>Blockquote</strong> Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sapien ut diam gravida, at faucibus justo tristique. Ut sed tempor ex, sit amet cursus ante. Nulla venenatis tincidunt arcu, non interdum nibh ultrices id. Nulla pellentesque mi ut nibh posuere sollicitudin.
+                    <strong className='strong'>{this.getIntlMessage('sg.typography.blockquote')}</strong> Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sapien ut diam gravida, at faucibus justo tristique. Ut sed tempor ex, sit amet cursus ante. Nulla venenatis tincidunt arcu, non interdum nibh ultrices id. Nulla pellentesque mi ut nibh posuere sollicitudin.
                 </blockquote>
 
                 <hr className='sg-hr' />
 
-                <h2 className='sg-h2'>{'Inline elements'}</h2>
+                <h2 className='sg-h2'>{this.getIntlMessage('sg.typography.inline-elements')}</h2>
                 <p className='p'>
-                    <a className='a'>{'Anchor'}</a>
-                    <strong className='strong'> Strong Tag</strong>, <b className='b'>B Tag</b>,
-                    <em className='em'> Em tag</em>, <i className='i'>I tag</i>,
-                    <sub className='sub'> Sub tag</sub>, <sup className='sup'>Sup tag</sup>,
-                    <dfn className='dfn'> Dfn tag</dfn>
+                    <a className='a'>{this.getIntlMessage('sg.typography.a')}</a>
+                    <strong className='strong'> {this.getIntlMessage('sg.typography.strong')}</strong>,
+                    <b className='b'> {this.getIntlMessage('sg.typography.b')}</b>,
+                    <em className='em'> {this.getIntlMessage('sg.typography.em')}</em>,
+                    <i className='i'> {this.getIntlMessage('sg.typography.i')}</i>,
+                    <sub className='sub'> {this.getIntlMessage('sg.typography.sub')}</sub>,
+                    <sup className='sup'> {this.getIntlMessage('sg.typography.sup')}</sup>,
+                    <dfn className='dfn'> {this.getIntlMessage('sg.typography.dfn')}</dfn>
                 </p>
             </div>
         );
