cdd1e8b313ac45307065d7749fc49b9d39c34ab5
Gordon Forsythe <gordon@contatta.com>
2015-02-23 16:30:57 -0700

    iterate through known languages from browser before checking en-us, then fall back to path key #75

--- application/ui/mixins/intlHelperMixin.js
+++ application/ui/mixins/intlHelperMixin.js
@@ -10,37 +10,56 @@ module.exports = {
      * Defaults to en-us if the message doesn't exist for the current language.
      * If neither is found, the final portion of the path is returned
      *
-     * @param  string path   The path of the message in the intl file
-     * @return string        Translated message
+     * @param  {string} path   The path of the message in the intl file
+     * @return {string}        Translated message
      */
     t : function(path)
     {
-        var messages  = this.props.messages || this.context.messages;
-        var langPath  = navigator.language.toLowerCase() + '.' + path;
-        var pathParts = langPath.split('.');
-
         var message;
+        var languages = typeof navigator.languages === 'undefined' ? [navigator.language] : navigator.languages;
 
-        try {
-            message = pathParts.reduce(function (obj, pathPart) {
-                return obj[pathPart];
-            }, messages);
+        // Iterate through each language to get a translated string
+        for (var i in languages) {
+            var lang = languages[i];
 
-            if (message === undefined) {
-                //Default to en-us, allows for partial page translation
-                langPath  = 'en-us.' + path;
-                pathParts = langPath.split('.');
-
-                message = pathParts.reduce(function (obj, pathPart) {
-                    return obj[pathPart];
-                }, messages);
-            }
-        } finally {
-            if (message === undefined) {
-                return pathParts[pathParts.length-1];
+            message = this._getTranslatedMessage(lang, path);
+            if (message) {
+                return message;
             }
         }
 
-        return message;
+        // check to see if en-us is part of the languages array.  If not, check for an english string
+        if (!navigator.languages.indexOf('en-us') && !navigator.languages.indexOf('en-US')) {
+            message = this._getTranslatedMessage('en-us', path);
+            if (message) {
+                return message;
+            }
+        }
+
+        // if a message was not found, return the end of the path
+        var pathParts = path.split('.');
+        return pathParts[pathParts.length-1];
+    },
+
+    /**
+     * Checks for a translation string using a specific locale and dot-namespaced identifier
+     * Returns false if not found
+     *
+     * @param {string} lang Locale string ("en-US", etc)
+     * @param {string} path Identifier ("test.My String")
+     * @returns {boolean|string}
+     */
+    _getTranslatedMessage: function(lang, path)
+    {
+        var langPath  = lang.toLowerCase() + '.' + path;
+        var pathParts = langPath.split('.');
+        var messages  = this.props.messages || this.context.messages;
+        try {
+            return pathParts.reduce(function (obj, pathPart) {
+                return obj[pathPart];
+            }, messages);
+        } catch(e) {
+            return false;
+        }
     }
 };
