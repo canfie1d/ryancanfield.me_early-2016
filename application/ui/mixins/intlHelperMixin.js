f8c6c5fd0db4aef0100eb71c931c12e367890664
Adam Gerbens <adam@synapsestudios.com>
2015-02-23 15:14:03 -0700

    Refs #75 | Update translate function to avoid using exceptions to control the flow

--- application/ui/mixins/intlHelperMixin.js
+++ application/ui/mixins/intlHelperMixin.js
@@ -8,21 +8,39 @@ module.exports = {
      * Translate function that returns a message from the intl file.
      * The message returned is dependent on the language of the browser.
      * Defaults to en-us if the message doesn't exist for the current language.
-     * If neither is found, the path is returned in curly braces
+     * If neither is found, the final portion of the path is returned
      *
      * @param  string path   The path of the message in the intl file
      * @return string        Translated message
      */
     t : function(path)
     {
+        var messages  = this.props.messages || this.context.messages;
+        var langPath  = navigator.language + '.' + path;
+        var pathParts = langPath.split('.');
+
+        var message;
+
         try {
-            return this.getIntlMessage(navigator.language + '.' + path);
-        } catch(e) {
-            try {
-                return this.getIntlMessage('en-us.' + path);
-            } catch (e) {
-                return '{' + path + '}';
+            message = pathParts.reduce(function (obj, pathPart) {
+                return obj[pathPart];
+            }, messages);
+
+            if (message === undefined) {
+                //Default to en-us, allows for partial page translation
+                langPath  = 'en-us.' + path;
+                pathParts = langPath.split('.');
+
+                message = pathParts.reduce(function (obj, pathPart) {
+                    return obj[pathPart];
+                }, messages);
+            }
+        } finally {
+            if (message === undefined) {
+                return pathParts[pathParts.length-1];
             }
         }
+
+        return message;
     }
 };
