cd9f0df84a4fc5df0d1da2633f33963c66daee2d
Kyle Cesmat <kyle@kylecesmat.com>
2016-01-07 15:43:51 -0700

    fix routing.

--- dev-server.js
+++ dev-server.js
@@ -40,6 +40,16 @@ app.use(appConfig.api.prefix, proxy('http://' + appConfig.proxy.hostname, {
     limit : '50mb'
 }));
 
+app.use(function (req, res, next) {
+    var ext = path.extname(req.url);
+
+    if ((ext === '' || ext === '.html') && req.url !== '/') {
+        req.pipe(request('http://' + req.hostname + ':9000')).pipe(res);
+    } else {
+        next();
+    }
+});
+
 app.listen(9000, function (err, result) {
     if (err) {
         console.log(err);
