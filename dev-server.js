e439a54e7b351a41cb3e0e8470ad037f8312f496
Ryan Canfield <ryan@synapsestudios.com>
2016-04-17 11:51:00 -0700

    merge update-template into master

--- dev-server.js
+++ dev-server.js
@@ -1,36 +1,29 @@
-global.__BACKEND__     = process.env.BACKEND;
+global.__BACKEND__ = process.env.BACKEND;
 global.__ENVIRONMENT__ = process.env.APP_ENV || 'development';
-global.__HOSTNAME__    = process.env.HOST || 'localhost';
-global.__APPTYPE__     = process.env.APP_TYPE || 'shared';
+global.__HOSTNAME__ = process.env.HOST || 'localhost';
 
-var getBasePathAndPort = function (appType) {
-    switch (appType) {
-        case 'shared':
-        case 'patternlib':
-        case 'pl':
-        case 'pattern-library':
-            return { path: 'shared', port: 9000 };
-        default:
-            throw new Error(`Unknown app type: ${appType}`);
-    }
-};
+var path          = require('path');
+var request       = require('request');
+var webpack       = require('webpack');
+var appConfig     = require('./application/config');
+var webpackConfig = require('./webpack.config');
+var proxy         = require('express-http-proxy');
+var express       = require('express');
+var url           = require('url');
 
-var appBase = getBasePathAndPort(__APPTYPE__);
-var __PORT__ = appBase.port || 9000;
+var app = express();
+var compiler = webpack(webpackConfig);
 
-var path             = require('path');
-var request          = require('request');
-var webpack          = require('webpack');
-var appConfig        = require('./config');
-var webpackConfig    = require('./webpack.config');
-var proxy            = require('express-http-proxy');
-var express          = require('express');
-var url              = require('url');
+app.use(require('webpack-dev-middleware')(compiler, {
+    contentBase: path.resolve(__dirname, 'build'),
+    hot: true,
+    quiet: false,
+    noInfo: true,
+    lazy: false,
+    stats: true,
+}));
 
-var app        = express();
-var compiler   = webpack(webpackConfig);
-var publicPath = webpackConfig[0].output.publicPath;
-
+app.use(require('webpack-hot-middleware')(compiler));
 
 if (!appConfig.api.prefix) {
     throw new Error('API prefix not set in configuration');
@@ -40,43 +33,26 @@ if (!appConfig.proxy.hostname) {
     throw new Error('API proxy hostname not set in configuration');
 }
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-app.use(appConfig.api.prefix, proxy('https://' + appConfig.proxy.hostname, {
+app.use(appConfig.api.prefix, proxy('http://' + appConfig.proxy.hostname, {
     limit: '50mb',
-    forwardPath: function(req, res) {
-        return require('url').parse(req.url).path;
-    },
 }));
-
-app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath: publicPath,
-    contentBase: path.resolve(__dirname, 'build/' + appBase.path),
-    hot: true,
-    quiet: false,
-    noInfo: false,
-    lazy: false,
-    stats: true,
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(function (req, res, next) {
     var urlPath = url.parse(req.url).pathname;
     var ext = path.extname(urlPath);
 
     if ((ext === '' || ext === '.html') && req.url !== '/') {
-        req.pipe(request('http://' + req.hostname + ':' + __PORT__)).pipe(res);
+        req.pipe(request('http://' + req.hostname + ':9000')).pipe(res);
     } else {
         next();
     }
 });
 
-app.listen(__PORT__, function (err, result) {
+app.listen(9000, function (err, result) {
     if (err) {
         console.log(err);
         return null;
     }
 
-    console.log('Listening at localhost:' + __PORT__);
+    console.log('Listening at localhost:9000');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,167 +1,118 @@
+/* eslint-disable */
 var __HOSTNAME__ = process.env.HOST ? process.env.HOST : 'localhost';
-var __APPTYPE__  = process.env.APP_TYPE || 'shared';
-
 var autoprefixer      = require('autoprefixer');
-var Webpack           = require('webpack');
+var webpack           = require('webpack');
 var WebpackError      = require('webpack-error-notification');
 var path              = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 var environment = (process.env.APP_ENV || 'development');
 var npmPath     = path.resolve(__dirname, 'node_modules');
-
-var getBasePathAndPort = function (appType) {
-    switch (appType) {
-        case 'shared':
-        case 'patternlib':
-        case 'pattern-library':
-        case 'pl':
-            return { path: 'shared', port: 9000 };
-        default:
-            throw new Error(`Unknown app type: ${appType}`);
-    }
-};
-
-var appBase = getBasePathAndPort(__APPTYPE__);
-var appFolder = './' + appBase.path;
-var __PORT__ = appBase.port;
-
 var config      = {
-    devtools: [],
-    entries: {
-        app: environment === 'development'
-            ? [appFolder + '/bootstrap.js', 'webpack-hot-middleware/client?path=/__webpack_hmr?http://' + __HOSTNAME__ + ':' + __PORT__]
-            : [appFolder + '/bootstrap.js'],
-        media: [appFolder + '/media.js']
-    },
-    plugins: [
-        new Webpack.DefinePlugin({
-            __BACKEND__: process.env.BACKEND ? '\'' + process.env.BACKEND + '\'' : undefined,
-            __ENVIRONMENT__: '\'' + environment + '\'',
-            __HOSTNAME__: '\'' + __HOSTNAME__ + '\'',
-            __APPTYPE__: '\'' + __APPTYPE__ + '\'',
-            'process.env': {
-                NODE_ENV: '\'' + environment + '\'',
-            },
-        }),
-        new Webpack.optimize.OccurrenceOrderPlugin(),
-        new Webpack.optimize.DedupePlugin(),
-        new HtmlWebpackPlugin({
-            favicon  : path.join(__dirname, 'static-media/icons/favicon.png'),
-            template: './' + appBase.path + '/index.html',
-            inject: false
-        }),
-    ],
-    reactLoaders: ['babel'],
-    sassOptions: (
-        '?outputStyle=' + (environment === 'production' ? 'compressed' : 'nested') +
-        '&includePaths[]=' + npmPath
-    ),
-    styleOptions: '?insertAt=top'
+  devtools : [],
+  entry    : environment !== 'production'
+    ? ['./application/bootstrap.js', 'webpack-hot-middleware/client?path=/__webpack_hmr?http://' + __HOSTNAME__ + ':9000']
+    : ['./application/bootstrap.js'],
+  media : ['./application/media.js'],
+  plugins  : [
+    new webpack.DefinePlugin({
+      __BACKEND__     : process.env.BACKEND ? '\'' + process.env.BACKEND + '\'' : undefined,
+      __ENVIRONMENT__ : '\'' + environment + '\'',
+      __HOSTNAME__    : '\'' + __HOSTNAME__ + '\'',
+      "process.env"   : {
+        NODE_ENV : '\'' + environment + '\''
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new HtmlWebpackPlugin({template : './application/index.html'})
+  ],
+  sassOptions  : (
+    '?outputStyle=' + (environment === 'production' ? 'compressed' : 'nested') +
+    '&includePaths[]=' + npmPath
+  )
 };
 
 if (environment !== 'production') {
-    config.devtools = '#inline-source-map';
+  config.devtools = 'eval-source-map';
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
 
-    config.plugins.push(
-        new Webpack.HotModuleReplacementPlugin(),
-        new Webpack.NoErrorsPlugin()
-    );
-
-    if (process.platform !== 'win32') {
-        config.plugins.push(new WebpackError(process.platform));
-    }
+  if (process.platform !== 'win32') {
+    config.plugins.push(new WebpackError(process.platform));
+  }
 }
 
-module.exports = [{
-    name: 'app bundle',
-    entry: config.entries.app,
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
+module.exports = [
+  {
+    name   : 'app bundle',
+    entry  : config.entry,
+    output : {
+      filename   : 'app.js',
+      path       : path.resolve(__dirname, 'build'),
+      publicPath : '/'
     },
     module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'eslint-loader',
-                exclude: npmPath,
-            },
-        ],
-        loaders: [
-            {
-                test: /\.(eot|ico|ttf|woff|woff2|gif|jpe?g|png|svg|mp3)$/,
-                loader: 'file-loader',
-                exclude: npmPath,
-            },
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel'],
-                exclude: npmPath,
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader',
-                exclude: npmPath,
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style' + config.styleOptions + '!css!postcss!sass' + config.sassOptions,
-            }
-        ],
-    },
-    plugins: config.plugins,
-    postcss: function () {
-        return [autoprefixer];
+      loaders : [
+        {
+          test   : /\.(eot|ico|ttf|woff|woff2|gif|jpe?g|png|svg|mp3)$/,
+          loader : 'file-loader',
+          exclude : npmPath
+        },
+        {
+          test    : /\.jsx?$/,
+          loaders : ['babel'],
+          exclude : npmPath
+        },
+        {
+          test   : /\.json$/,
+          loader : 'json-loader',
+          exclude : npmPath
+        },
+        {
+          test   : /\.scss$/,
+          loader : 'style!css!postcss!sass' + config.sassOptions
+        }
+      ]
     },
 
-    resolve: {
-        alias: {
-            shared: path.resolve('./shared')
+    plugins : config.plugins,
+    postcss : function() {
+      return [autoprefixer];
+    },
+    resolve : {
+      extensions : ['', '.css', '.js', '.json', '.jsx', '.scss', '.webpack.js', '.web.js']
+    },
+    devtool : config.devtools
+  },
+  {
+    name   : 'media bundle',
+    entry  : config.media,
+    output : {
+      filename   : 'media.js',
+      path       : path.resolve(__dirname, 'build')
+    },
+    module : {
+      loaders : [
+        {
+          test   : /\.(eot|ico|ttf|woff|woff2|gif|jpe?g|png|svg)$/,
+          loader : 'file-loader'
         },
-        extensions: ['', '.css', '.js', '.json', '.jsx', '.scss', '.webpack.js', '.web.js'],
+        {
+          test   : /\.scss$/,
+          loader : 'style!css!postcss!sass' + config.sassOptions
+        }
+      ]
     },
-    devtool: config.devtools,
-}, {
-    name: 'media bundle',
-    entry: config.entries.media,
-    output: {
-        filename: 'media.js',
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
+    plugins : config.plugins,
+    postcss : function() {
+      return [autoprefixer];
     },
-    module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'eslint-loader',
-                exclude: npmPath,
-            },
-        ],
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel'],
-                exclude: npmPath,
-            },
-            {
-                test: /\.(eot|ico|ttf|woff|woff2|gif|jpe?g|png|svg)$/,
-                loader: 'file-loader',
-                exclude: npmPath,
-            },
-            {
-                test   : /\.s?css$/,
-                loader : 'style!css!postcss!sass' + config.sassOptions
-            },
-        ],
-        plugins: [],
+    resolve : {
+      extensions : ['', '.css', '.js', '.scss', '.webpack.js', '.web.js']
     },
-    resolve: {
-        alias: {
-            shared: path.resolve('./shared')
-        },
-        extensions: ['', '.css', '.js', '.json', '.jsx', '.scss', '.webpack.js', '.web.js'],
-    },
-    devtool: config.devtools,
-}];
+    devtool : config.devtools
+  }
+]
+/* eslint-enable */
