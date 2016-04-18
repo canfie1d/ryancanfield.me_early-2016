faf8f0b791a4a53fd7e54c5bebde049239994853
Ryan Canfield <ryan@synapsestudios.com>
2016-04-18 09:48:24 -0700

    add outline hiding

--- shared/ui/layouts/site.jsx
+++ shared/ui/layouts/site.jsx
@@ -5,6 +5,42 @@ const site = React.createClass({
 
     displayName: 'SiteLayout',
 
+    getInitialState() {
+        return {
+            hideFocusOutline : true
+        };
+    },
+
+    componentDidMount()
+    {
+        window.addEventListener('keydown', this.showFocusOutline);
+        window.addEventListener('mousemove', this.hideFocusOutline);
+    },
+
+    componentWillUnmount()
+    {
+        window.removeEventListener('keydown', this.showFocusOutline);
+        window.removeEventListener('mousemove', this.hideFocusOutline);
+    },
+
+    showFocusOutline(event)
+    {
+        if (this.state.hideFocusOutline && event.keyCode === 9) {
+            this.setState ({
+                hideFocusOutline : false
+            });
+        }
+    },
+
+    hideFocusOutline()
+    {
+        if (!this.state.hideFocusOutline) {
+            this.setState ({
+                hideFocusOutline : true
+            });
+        }
+    },
+
     render() {
 
         let currentPage = window.location.pathname,
@@ -35,6 +71,7 @@ const site = React.createClass({
 
         const classes = [
             'l--site-wrapper',
+            this.state.hideFocusOutline ? 'focus-outline--hidden' : null,
             pageClass
         ].join(' ').trim();
 
