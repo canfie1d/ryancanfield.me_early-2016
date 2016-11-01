cd0bc0d4d5468b6ab14d56e4a074425e36498e93
Ryan Canfield <ryancanfield@level11.com>
2016-10-31 17:49:27 -0700

    Remove testing functionality and pattern library

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Inspect state, time travel through various actions. Enabled by default for devel
 
 ### Initializing the Development Environment
 1. `npm install` to add dependencies
-1. `npm start` to build the app and launch a server at `localhost:9000`
+1. `npm run start:shared` to build the app and launch a server at `localhost:9000`
 
 ### Builds
 1. `npm run dist` to build a production bundle
@@ -21,8 +21,3 @@ Inspect state, time travel through various actions. Enabled by default for devel
 
 ### Testing on a network
 1. `env HOST=LOCAL_IP npm start`
-
-### Running Tests
-1. Run React tests with gulp at `localhost:9001` with `npm test`
-1. Run React tests with Karma in Chrome and Firefox with `npm run karma`
-1. Run React tests from the command line with `./node_modules/karma/bin/karma start --single-run --browsers PhantomJS`
--- shared/routes.js
+++ shared/routes.js
@@ -8,34 +8,17 @@ import ConceptsPage from './ui/pages/concepts';
 import ProjectsPage from './ui/pages/projects';
 import AboutPage from './ui/pages/about';
 import ElsewherePage from './ui/pages/elsewhere';
-import PatternLibraryPage from './ui/pages/pattern-library';
 import NotFoundPage from './ui/pages/404';
 import SecretPage from './ui/pages/secret';
 
-function getEnvironmentDependentRoutes() {
-    let routes = [];
-
-    if (__ENVIRONMENT__ !== 'production') {
-        routes = routes.concat([
-            <Route path="/pattern-library/:section"
-                component={PatternLibraryPage} key="pattern-library-section"
-            />,
-            <Redirect from="/pattern-library" key="pattern-library" to="/pattern-library/all"/>,
-        ]);
-    }
-
-    return routes;
-}
-
 export default (
     <Route path="/" component={SiteLayout}>
-        <IndexRoute component={MenuPage}/>
-        <Route path="/projects" component={ProjectsPage}/>
-        <Route path="/concepts" component={ConceptsPage}/>
-        <Route path="/about" component={AboutPage}/>
-        <Route path="/elsewhere" component={ElsewherePage}/>
-        <Route path="/secret" component={SecretPage}/>
-        {getEnvironmentDependentRoutes()}
-        <Route path="*" component={NotFoundPage}/>
+        <IndexRoute component={MenuPage} />
+        <Route path="/projects" component={ProjectsPage} />
+        <Route path="/concepts" component={ConceptsPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/elsewhere" component={ElsewherePage} />
+        <Route path="/secret" component={SecretPage} />
+        <Route path="*" component={NotFoundPage} />
     </Route>
 );
